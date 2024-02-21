f53876b4f7de51914939250bc0232218c836a13c
emil <emiljbb@gamil.com>
2024-02-21 22:38:44 +0100

    pagina version 2

--- cues.js
+++ cues.js
@@ -2,3 +2,16 @@ function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('oculto');
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var toggle = document.getElementById('toggle');
+    toggle.addEventListener('change', function() {
+        var lista = document.querySelector('.lista');
+        if (this.checked) {
+            lista.style.display = 'block';
+        } else {
+            lista.style.display = 'none';
+        }
+    });
+});
